9b54aebaa02d5ef26db66583850f102daa1d9a67
Arek Olek <arek@applicake.com>
2013-04-27 13:39:23 +0200

    just stuff

--- src/test/java/com/github/arekolek/sarenka/prefs/AlarmPrefsTest.java
+++ src/test/java/com/github/arekolek/sarenka/prefs/AlarmPrefsTest.java
@@ -1,11 +1,8 @@
 package com.github.arekolek.sarenka.prefs;
 
-import com.xtremelabs.robolectric.RobolectricTestRunner;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(RobolectricTestRunner.class)
+
 public class AlarmPrefsTest {
 
     @Test
