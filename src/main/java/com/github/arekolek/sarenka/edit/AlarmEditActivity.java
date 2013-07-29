8a1e232f21c42c5dc64a479d8ea60fb311fb2653
Arek Olek <arek@applicake.com>
2013-07-29 12:16:01 +0200

    show a toast "alarm set for..."

--- src/main/java/com/github/arekolek/sarenka/edit/AlarmEditActivity.java
+++ src/main/java/com/github/arekolek/sarenka/edit/AlarmEditActivity.java
@@ -12,6 +12,7 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.view.*;
 import android.widget.Switch;
+import android.widget.Toast;
 import com.github.arekolek.sarenka.R;
 import com.github.arekolek.sarenka.ring.Alarms;
 
@@ -109,9 +110,53 @@ public class AlarmEditActivity extends Activity {
             alarm.sound = prefs.getSound();
             alarm.enabled = prefs.isEnabled();
 
-            Alarms.saveAlarm(getActivity(), alarm);
+            long time = Alarms.saveAlarm(getActivity(), alarm);
+
+            if (prefs.isEnabled()) {
+                popAlarmSetToast(getActivity(), time);
+            }
 
             getActivity().finish();
+        }
+
+        public static void popAlarmSetToast(Context context, long timeInMillis) {
+            String toastText = formatToast(context, timeInMillis);
+            Toast.makeText(context, toastText, Toast.LENGTH_LONG).show();
+        }
+
+        /**
+         * format "Alarm set for 2 days 7 hours and 53 minutes from
+         * now"
+         */
+        static String formatToast(Context context, long timeInMillis) {
+            long delta = timeInMillis - System.currentTimeMillis();
+            long hours = delta / (1000 * 60 * 60);
+            long minutes = delta / (1000 * 60) % 60;
+            long days = hours / 24;
+            hours = hours % 24;
+
+            String daySeq = (days == 0) ? "" :
+                    (days == 1) ? context.getString(R.string.day) :
+                            context.getString(R.string.days, Long.toString(days));
+
+            String minSeq = (minutes == 0) ? "" :
+                    (minutes == 1) ? context.getString(R.string.minute) :
+                            context.getString(R.string.minutes, Long.toString(minutes));
+
+            String hourSeq = (hours == 0) ? "" :
+                    (hours == 1) ? context.getString(R.string.hour) :
+                            context.getString(R.string.hours, Long.toString(hours));
+
+            boolean dispDays = days > 0;
+            boolean dispHour = hours > 0;
+            boolean dispMinute = minutes > 0;
+
+            int index = (dispDays ? 1 : 0) |
+                    (dispHour ? 2 : 0) |
+                    (dispMinute ? 4 : 0);
+
+            String[] formats = context.getResources().getStringArray(R.array.alarm_set);
+            return String.format(formats[index], daySeq, hourSeq, minSeq);
         }
 
         @Override
