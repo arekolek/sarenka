adfd01c27c71f4b7b74298cac9f4c590d212b234
Arek Olek <arek@applicake.com>
2013-07-31 13:28:17 +0200

    Cursor position in alarm label should be at the end

--- src/main/java/com/github/arekolek/sarenka/edit/EditTextPreference2.java
+++ src/main/java/com/github/arekolek/sarenka/edit/EditTextPreference2.java
@@ -1,13 +1,29 @@
 package com.github.arekolek.sarenka.edit;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
 import android.preference.EditTextPreference;
 import android.util.AttributeSet;
+import android.widget.EditText;
 
 public class EditTextPreference2 extends EditTextPreference {
 
     public EditTextPreference2(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        Handler delayedRun = new Handler();
+        delayedRun.post(new Runnable() {
+            @Override
+            public void run() {
+                EditText textBox = getEditText();
+                textBox.setSelection(0, textBox.getText().length());
+            }
+        });
     }
 
     public void show() {
